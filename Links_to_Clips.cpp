894de59427a524cee49f1361d1318536c4680d68
m-riss <95452643+m-riss@users.noreply.github.com>
2022-09-14 08:25:33 -0400

    Create Links_to_Clips.cpp

---
+++ Links_to_Clips.cpp
@@ -0,0 +1,245 @@
+#include <iostream>
+#include <cmath>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+
+int clipnumber = 0;
+int Exit = 0;
+
+cout << "Which Subathon Clip Would You Like To Watch" << "?" << endl;
+cin >> clipnumber;
+
+if (clipnumber == 1) {
+cout << "https://clips.twitch.tv/SmokyAlluringCobblerKreygasm-dowyaXE97YzOhQuS";
+}
+
+if (clipnumber == 2) {
+cout << "https://clips.twitch.tv/ElegantCogentSproutSquadGoals-CkougUpO9njqX6XK";
+}
+
+if (clipnumber == 3) {
+cout << "https://clips.twitch.tv/ScrumptiousComfortableOrangePunchTrees-BJ7MRXSAuDvZrsc2";
+}
+
+if (clipnumber == 4) {
+cout << "https://clips.twitch.tv/LuckyMushyHyenaKAPOW--Gg-UVY_wmHRdjMa";
+}
+
+if (clipnumber == 5) {
+cout << "https://clips.twitch.tv/ZealousGoodHamsterWutFace-UtCiwlmSBCvWnWkp";
+}
+
+if (clipnumber == 6) {
+cout << "https://clips.twitch.tv/IronicManlyDugongArsonNoSexy-3-o2mwCPLEH2fr1v";
+}
+
+if (clipnumber == 7) {
+cout << "https://clips.twitch.tv/LitigiousCleanKoupreyCoolCat-3t78hlYJFv8CIE8n";
+}
+
+if (clipnumber == 8) {
+cout << "https://clips.twitch.tv/VenomousBloodyCucumberAMPEnergy-NIS7nmlXgaD3Szgv";
+}
+
+if (clipnumber == 9) {
+cout << "https://clips.twitch.tv/YummyCarelessMeerkatKAPOW-MjIKsQJd9HA_rdOi";
+}
+
+if (clipnumber == 10) {
+cout << "https://clips.twitch.tv/DignifiedNimblePidgeonKevinTurtle-jRM5ySk5_QE38t8I";
+}
+
+
+cout << "https://clips.twitch.tv/LachrymoseHomelyCougarWoofer-miLPgCIArE7vkpFO";
+
+
+
+cout << "https://clips.twitch.tv/FilthyPeacefulAubergineGingerPower-o3-fEBD-ftV1L1hd";
+
+
+
+cout << "https://clips.twitch.tv/FlirtySavageDiamondUnSane-2GX24KN8GOCFe3ji";
+
+
+
+cout << "https://clips.twitch.tv/WimpyVictoriousLarkSaltBae-yoQhUpNq0s4GOk94";
+
+
+
+cout << "https://clips.twitch.tv/UnusualObservantBananaBCWarrior-nnTvxVKvBNQTjjLF";
+
+
+
+cout << "https://clips.twitch.tv/PoisedHomelyCourgetteSSSsss-qCY3pfJrdNHgx3mx";
+
+
+
+cout << "https://clips.twitch.tv/DifficultCaringCheddarHeyGuys-V8qyiVAqg1d_paJa";
+
+
+
+cout << "https://clips.twitch.tv/StrongTangibleJamCoolCat-NGTcyfFAOmP04ziY";
+
+
+
+cout << "https://clips.twitch.tv/ClearBraveDolphinTBTacoLeft-OurwATa0jNvrtZ3d";
+
+
+
+cout << "https://clips.twitch.tv/SparklyEphemeralBasenjiPraiseIt-9rbOvdnXl6jk1vTB";
+
+
+
+cout << "https://clips.twitch.tv/CharmingAggressiveShieldCorgiDerp-Z3X_8fVYii1CDhTH";
+
+
+
+cout << "https://clips.twitch.tv/GrotesqueSavoryKoupreyFunRun-YeLYHpMsK_mLsqnt";
+
+
+
+cout << "https://clips.twitch.tv/DelightfulOpenSoybeanTF2John-dWnBhN6T2OfbHMcn";
+
+
+
+cout << "https://clips.twitch.tv/HelpfulPleasantElephantSmoocherZ-YTLd63XmY50Qszlo";
+
+
+
+cout << "https://clips.twitch.tv/DiligentBovineSpindleFrankerZ-_mDKv5EbByBNdczB";
+
+
+
+cout << "https://clips.twitch.tv/ShyHelpfulKoupreyUnSane-nerBJPie3CrY7Wfd";
+
+
+
+cout << "https://clips.twitch.tv/NaiveMistyLionDuDudu-yPZHktta4oKW6zsD";
+
+
+
+cout << "https://clips.twitch.tv/ExquisiteAbstruseYamVoteYea-c9hD-ak27zO3cnhd";
+
+
+
+cout << "https://clips.twitch.tv/CreativeSuspiciousWhaleMingLee-N8BMqAEROZz-ExfC";
+
+
+
+cout << "https://clips.twitch.tv/SpoopyCheerfulOcelotM4xHeh-SX0WP4U_COsOS_e-";
+
+
+
+cout << "https://clips.twitch.tv/FaintMoralGuanacoStrawBeary--Oblm6Tl9zyD6eZE";
+
+
+
+cout << "https://clips.twitch.tv/EnjoyableFuriousClintCoolCat-YSnDHT23x-un9y7L";
+
+
+
+cout << "https://clips.twitch.tv/AnnoyingGoodAsparagusYee-VWsoHHKIWTxqfjpQ";
+
+
+
+cout << "https://clips.twitch.tv/UninterestedGrossAlbatrossWutFace-K36x0RBXrlNhTGr4";
+
+
+
+cout << "https://clips.twitch.tv/KnottySmoggyNigiriYouWHY-I86iO9RfAW3QSYk1";
+
+
+
+cout << "https://clips.twitch.tv/FancyAnnoyingReubenKeyboardCat-tBxGMuD09De8kyHt";
+
+
+
+cout << "https://clips.twitch.tv/ClumsyMistyChimpanzeePMSTwin-SJsF3CjB5zIFQaZ5";
+
+
+
+cout << "https://clips.twitch.tv/SpicyPiercingCheetahPJSalt-Fe2IQx1Av4FMAC5n";
+
+
+
+cout << "https://clips.twitch.tv/VainNastyCoffeeNomNom-Jeo0VLVZFbpa1qQA";
+
+
+
+cout << "https://clips.twitch.tv/SoftNimbleDelicataImGlitch-oMPeBEBmiPevpz2D";
+
+
+
+cout << "https://clips.twitch.tv/ExpensiveRichRaccoonCmonBruh-A3-BHMLPY3vN9FnN";
+
+
+
+cout << "https://clips.twitch.tv/TallFaintCodRaccAttack-K1HFDVeZns0zXajS";
+
+
+
+cout << "https://clips.twitch.tv/ArbitraryPuzzledRedpandaPicoMause-qkdU8Sc02_1_LILC";
+
+
+
+cout << "https://clips.twitch.tv/ResoluteBashfulZucchiniMoreCowbell-RzsrUXOG1LB6JkTR";
+
+
+
+cout << "https://clips.twitch.tv/TalentedGenerousWeaselPunchTrees-b9BAEmJNYCvwpCuE";
+
+
+
+cout << "https://clips.twitch.tv/ShySucculentAubergineUnSane-f0wRF5zkMfa4X5R_";
+
+
+
+cout << "https://clips.twitch.tv/ResourcefulFreezingSalamanderOSkomodo-SwQf35YKkdgByI1o";
+
+
+
+cout << "https://clips.twitch.tv/DifficultSourGarbageAMPTropPunch-PLn_CWX7cQ5k3eCF";
+
+
+
+cout << "https://clips.twitch.tv/CrackyHedonisticCobraPrimeMe-wvG3pKNW6b2p9DL2";
+
+
+
+cout << "https://clips.twitch.tv/BreakableStupidRedpandaOMGScoots-HkjkAMqV-YczZ5cj";
+
+
+
+cout << "https://clips.twitch.tv/SnappyGoldenWallabyHoneyBadger-FwNTtenLfVdh9CUc";
+
+
+
+
+cout << "https://clips.twitch.tv/CrazyEnthusiasticPheasantThunBeast-_NVc3L_5L9qrah0g";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cout << "Would you like to Exit?" << endl;
+cin >> Exit;
+
+return 0;
+}
